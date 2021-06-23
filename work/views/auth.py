ee2d8fcca9d681c27a663d9ac41a1cc8692b0bf0
itmanharry <jack_chengqian@163.com>
2021-06-23 15:51:19 +0800

    finish login development

--- work/views/auth.py
+++ work/views/auth.py
@@ -1,5 +1,18 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, jsonify, request
+from work.models import User
 bp_auth = Blueprint('auth', __name__)
 @bp_auth.route('/login')
 def login():
     return render_template('auth/_login.html')
+@bp_auth.route('/enter', methods=['POST'])
+def enter():
+    data = request.get_json()
+    code = data['code']
+    password = data['password']
+    user = User.query.filter_by(code=code.lower()).first()
+    if user:
+        if not user.validate_password(password):
+            return jsonify(code=0, message='密码错误!!!')
+    else:
+        return jsonify(code=0, message='用户不存在!!!')
+    return jsonify(code=1, message='OK')
