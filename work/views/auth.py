7d6e9702fca598bea95102d2b9755908995ad64c
itmanharry <jack_chengqian@163.com>
2021-06-28 16:38:58 +0800

    i18n

--- work/views/auth.py
+++ work/views/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, jsonify, request
 from flask_login import login_user, logout_user
+from flask_babel import _
 from work.models import User
 bp_auth = Blueprint('auth', __name__)
 @bp_auth.route('/login')
@@ -17,6 +18,7 @@ def enter():
     else:
         return jsonify(code=0, message='用户不存在!!!')
     login_user(user, True) #交由flask_login管理登录后的信息
+    print(_('auth.login.success'))
     return jsonify(code=1, message='OK')
 @bp_auth.route('/exit')
 def exit():
